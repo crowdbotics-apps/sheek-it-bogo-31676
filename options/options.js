4127862e9717f617024cb7eca399448f22194575
Crowdbotics <team@crowdbotics.com>
2021-10-24 17:38:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "sheek_it_bogo_31676",
+  url: "https://sheek_it_bogo_31676.botics.co",
+  api: "https://sheek_it_bogo_31676.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
